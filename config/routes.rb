4c184575059fedb6086957621c383e14aad227f7
toneymar <toneymar@grinnell.edu>
2020-02-09 15:53:49 -0600

    complete hw3

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
+  get  '/about',   to: 'static_pages#about'
 
-  get 'static_pages/home'
+  get  '/contact', to: 'static_pages#contact'
 
-  get 'static_pages/help'
+  get '/help', to: 'static_pages#help'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
