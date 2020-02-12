45f7778813940c9ba50ccf5d1c4a5cab536ee704
toneymar <toneymar@grinnell.edu>
2020-02-12 14:22:46 -0600

    got dice roller working

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,16 @@
 class StaticPagesController < ApplicationController
 
   def home
-    @result = (1..6).to_a.shuffle.first
   end
 
   def help
   end
 
   def about
+  end
+
+  def roll
+    @result = (1..6).to_a.shuffle.first
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get '/help', to: 'static_pages#help'
 
+  get '/roll', to: 'static_pages#roll'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 end
