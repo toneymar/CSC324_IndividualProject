b3e53fdcaaecfa198040e1fcf6fa15c5aaa6cd30
toneymar <toneymar@grinnell.edu>
2020-02-23 15:04:04 -0600

    Added a test for the roll page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,4 +19,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get roll" do
+    get roll_path
+    assert_response :success
+    assert_select "title", "Roll | Ruby on Rails Tutorial Sample App"
+  end
+
 end
